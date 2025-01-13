437a147861ae7b1e7871d947ce0cbc9056a73230
Rainers Krumins <rainerkrum@gmail.com>
2025-01-13 22:08:48 +0200

    yank & show error

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,11 @@
 require("config.options")
 require("config.lazy")
+
+vim.api.nvim_set_keymap('v', '<C-c>', '"+y', { desc = 'Yank to system clipbaord' })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
