989ace524f01b3360668886e64c314dd96d6505f
Rainers K. <rainerkrum@gmail.com>
2025-03-21 13:45:20 +0200

    nvim format on save

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,6 +2,7 @@ require("custom.options")
 require("custom.lazy")
 
 vim.api.nvim_set_keymap('v', '<C-c>', '"+y', { desc = 'Yank to system clipbaord' })
+
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
@@ -11,3 +12,11 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear highlight search'})
+
+-- Fomat on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.rs", "*.go" },
+  callback = function()
+    vim.lsp.buf.format({ async = false })
+  end,
+})
