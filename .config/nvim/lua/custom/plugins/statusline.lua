67743f9914f913d40c1a81d7466e15f96cc371d0
Rainers K. <rainerkrum@gmail.com>
2025-03-21 14:40:07 +0200

    testing new statusline config

--- .config/nvim/lua/custom/plugins/statusline.lua
+++ .config/nvim/lua/custom/plugins/statusline.lua
@@ -2,46 +2,76 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-	    require('lualine').setup {
-		    options = {
-			    icons_enabled = true,
-			    theme = 'auto',
-			    component_separators = { left = '', right = ''},
-			    section_separators = { left = '', right = ''},
-			    disabled_filetypes = {
-				    statusline = {},
-				    winbar = {},
-			    },
-			    ignore_focus = {},
-			    always_divide_middle = true,
-			    always_show_tabline = true,
-			    globalstatus = false,
-			    refresh = {
-				    statusline = 100,
-				    tabline = 100,
-				    winbar = 100,
-			    }
-		    },
-		    sections = {
-			    lualine_a = {'mode'},
-			    lualine_b = {'branch', 'diff', 'diagnostics'},
-			    lualine_c = {'filename'},
-			    lualine_x = {'encoding', 'fileformat', 'filetype'},
-			    lualine_y = {'progress'},
-			    lualine_z = {'location'}
-		    },
-		    inactive_sections = {
-			    lualine_a = {},
-			    lualine_b = {},
-			    lualine_c = {'filename'},
-			    lualine_x = {'location'},
-			    lualine_y = {},
-			    lualine_z = {}
-		    },
-		    tabline = {},
-		    winbar = {},
-		    inactive_winbar = {},
-		    extensions = {}
-	    }
+		local icon = require("custom.icons")
+		local lualine = require("lualine")
+		local theme = require("custom.theme")
+
+		local filetype = { "filetype", icon_only = true }
+
+		local lsp_status = {
+			"lsp_status",
+			icon = "", -- f013
+			symbols = {
+				spinner = icon.spinner,
+				done = false,
+				separator = " ",
+			},
+			-- List of LSP names to ignore (e.g., `null-ls`):
+			ignore_lsp = {},
+		}
+
+		local diagnostics = {
+			"diagnostics",
+			sources = { "nvim_diagnostic" },
+			sections = { "error", "warn", "info", "hint" },
+			symbols = {
+				error = icon.diagnostics.Error,
+				hint = icon.diagnostics.Hint,
+				info = icon.diagnostics.Info,
+				warn = icon.diagnostics.Warning,
+			},
+			colored = true,
+			update_in_insert = false,
+			always_visible = false,
+		}
+
+		local diff = {
+			"diff",
+			source = function()
+				local gitsigns = vim.b.gitsigns_status_dict
+				if gitsigns then
+					return {
+						added = gitsigns.added,
+						modified = gitsigns.changed,
+						removed = gitsigns.removed,
+					}
+				end
+			end,
+			symbols = {
+				added = icon.git.LineAdded .. " ",
+				modified = icon.git.LineModified .. " ",
+				removed = icon.git.LineRemoved .. " ",
+			},
+			colored = true,
+			always_visible = false,
+		}
+
+		lualine.setup({
+			options = {
+				theme = theme.theme(),
+				globalstatus = true,
+				section_separators = "",
+				component_separators = "",
+				disabled_filetypes = { statusline = { "dashboard", "lazy", "alpha" } },
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = {},
+				lualine_c = { "filename", lsp_status },
+				lualine_x = { diff, diagnostics, filetype },
+				lualine_y = {},
+				lualine_z = {},
+			},
+		})
     end,
 }
