11cd767e3f2a62b9fb5d3830bdf557aa833bd58b
Rainers K. <rainerkrum@gmail.com>
2026-04-27 20:06:43 +0300

    nvim better options

--- .config/nvim/lua/custom/options.lua
+++ .config/nvim/lua/custom/options.lua
@@ -6,24 +6,28 @@ vim.g.maplocalleader = ' '
 -- Tabs
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true -- Copy indent from current line
 
 -- Line Numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
 
--- Nerd Font
-vim.g.have_nerd_font = true
+vim.opt.ignorecase = true -- case insensitive search
+vim.opt.smartcase = true -- case sensitive search if search term includes uppercase
 
--- Save undo history
-vim.opt.undofile = true
+vim.opt.undofile = true -- Save undo history
 
--- Show which line your cursor is on
-vim.opt.cursorline = false
+vim.opt.cursorline = false -- highlight current line
 
--- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = 'yes' -- Keep signcolumn on by default
 
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
-vim.opt.smartindent = true
+vim.g.have_nerd_font = true -- Nerd font
+
+vim.opt.autoread = true -- auto-reload if changes outside of nvim
+
+vim.opt.iskeyword:append("-") -- include '-' in words
